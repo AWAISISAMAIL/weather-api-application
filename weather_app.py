77ab8dbddee87ee1feb3dfedde5c44aee75aef8a
AWAISISMAIL <777awais121@gmail.com>
2026-07-13 19:53:19 +0500

    Fixed issue in code

--- weather_app.py
+++ weather_app.py
@@ -1,66 +1,101 @@
 import requests
-# finding lat/long from city name
+
+# API URLs
 GEO_URL = "https://geocoding-api.open-meteo.com/v1/search"
-# using lat/long for finding weather forcast
 WEATHER_URL = "https://api.open-meteo.com/v1/forecast"
-city = input("ENTER CITY NAME : ").strip()
-if not city:
-    print("ERROR! PLEASE ENTER CITY NAME BECAUSE IT IS IMPORTAN")
+
+
+# User input
+city = input("ENTER CITY NAME: ").strip()
+
+if city == "":
+    print("ERROR! PLEASE ENTER CITY NAME")
     exit()
-geo_params={"name":city,
-            "count":1,
-            "language":"en"}
+
+
+# Finding latitude and longitude
+geo_params = {
+    "name": city,
+    "count": 1,
+    "language": "en"
+}
+
+
 try:
     geo_response = requests.get(
         GEO_URL,
         params=geo_params,
         timeout=10
     )
+
 except requests.exceptions.RequestException:
     print("Internet or server connection error!")
     exit()
-geo_response=requests.get(GEO_URL,params=geo_params)
-if geo_response.status_code !=200:
-    print("SERVER CONNECTION ERROR ")
+
+
+if geo_response.status_code != 200:
+    print("SERVER CONNECTION ERROR")
     exit()
+
+
 geo_data = geo_response.json()
-# cheacking if city was actually found
+
+
+# Checking city exists
 if "results" not in geo_data:
-    print(f"Error: City '{city}' not found. Enter valid city name!")
+    print(f"Error: City '{city}' not found!")
     exit()
+
+
 city_info = geo_data["results"][0]
+
 lat = city_info["latitude"]
 lon = city_info["longitude"]
-country = city_info.get("country","")
+country = city_info.get("country", "Unknown")
+
+
+# Weather API request
 weather_params = {
     "latitude": lat,
     "longitude": lon,
     "current_weather": True
 }
+
+
 try:
     weather_response = requests.get(
         WEATHER_URL,
         params=weather_params,
         timeout=10
     )
+
 except requests.exceptions.RequestException:
     print("Unable to fetch weather data!")
     exit()
 
-if weather_response.status_code !=200:
-    print("Error: Something wents wrong!")
+
+if weather_response.status_code != 200:
+    print("Weather API error!")
     exit()
 
+
 weather_data = weather_response.json()
+
 current = weather_data["current_weather"]
-temp = current["temperature"]
-wind = current["windspeed"]
-is_day = current.get("is_day",1)
-#conditon display
+
+
+temperature = current["temperature"]
+wind_speed = current["windspeed"]
+is_day = current.get("is_day", 1)
+
+
 day_night = "DAY ☀️" if is_day == 1 else "NIGHT 🌙"
-print("\n-----------------------------------------------------")
-print(f"📍 Location: {city.capitalize()},{country}")
-print(f"🌡️ Temperature: {temp}C")
-print(f"💨 Wind Speed: {wind} km/h")
-print(f"🕰️ Time Period: {day_night}")
-print("-------------------------------------------------------")
+
+
+# Final Output
+print("\n------------------------------------")
+print(f"📍 Location: {city.capitalize()}, {country}")
+print(f"🌡️ Temperature: {temperature}°C")
+print(f"💨 Wind Speed: {wind_speed} km/h")
+print(f"🕒 Time: {day_night}")
+print("------------------------------------")
